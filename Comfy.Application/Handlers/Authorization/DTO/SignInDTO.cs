f140a9d6e4e6da8bec89af534740935535ffb89f
tapeex <1lavabox1@gmail.com>
2023-05-31 18:52:00 +0300

    Поменял местами  RefreshToken и AccessToken в SignInDTO

--- Comfy.Application/Handlers/Authorization/DTO/SignInDTO.cs
+++ Comfy.Application/Handlers/Authorization/DTO/SignInDTO.cs
@@ -3,6 +3,6 @@
 public sealed record SignInDTO
 {
     public Guid UserId { get; init; }
-    public string AccessToken { get; init; } = null!;
     public Guid RefreshToken { get; init; }
+    public string AccessToken { get; init; } = null!;
 }
