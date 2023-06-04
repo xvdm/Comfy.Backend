368e37f9891d999e1e11020c48fd6c4b872d8325
tapeex <1lavabox1@gmail.com>
2023-06-04 18:28:03 +0300

    Добавил тесты на получение пользователя

---
+++ Comfy.Tests/Mock/UserManagerMock.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System.Text;
+
+namespace Comfy.Tests.Mock;
+
+public static class UserManagerMock
+{
+    public static UserManager<TUser> TestUserManager<TUser>(IUserStore<TUser>? store = null) where TUser : class
+    {
+        store ??= new Mock<IUserStore<TUser>>().Object;
+        var options = new Mock<IOptions<IdentityOptions>>();
+        var idOptions = new IdentityOptions();
+        idOptions.Lockout.AllowedForNewUsers = false;
+        options.Setup(o => o.Value).Returns(idOptions);
+        var userValidators = new List<IUserValidator<TUser>>();
+        var validator = new Mock<IUserValidator<TUser>>();
+        userValidators.Add(validator.Object);
+        var pwdValidators = new List<PasswordValidator<TUser>>();
+        pwdValidators.Add(new PasswordValidator<TUser>());
+        var userManager = new UserManager<TUser>(store, options.Object, new PasswordHasher<TUser>(),
+            userValidators, pwdValidators, MockLookupNormalizer(),
+            new IdentityErrorDescriber(), null,
+            new Mock<ILogger<UserManager<TUser>>>().Object);
+        validator.Setup(v => v.ValidateAsync(userManager, It.IsAny<TUser>()))
+            .Returns(Task.FromResult(IdentityResult.Success)).Verifiable();
+        return userManager;
+    }
+
+    public static ILookupNormalizer MockLookupNormalizer()
+    {
+        var normalizerFunc = new Func<string?, string>(i =>
+        {
+            if (i is null) return null!;
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(i)).ToUpperInvariant();
+        });
+        var lookupNormalizer = new Mock<ILookupNormalizer>();
+        lookupNormalizer.Setup(i => i.NormalizeName(It.IsAny<string>())).Returns(normalizerFunc);
+        lookupNormalizer.Setup(i => i.NormalizeEmail(It.IsAny<string>())).Returns(normalizerFunc);
+        return lookupNormalizer.Object;
+    }
+}
---
+++ Comfy.Tests/Users/GetUserQueryHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Comfy.Application.Handlers.Users;
+using Comfy.Application.Handlers.Users.DTO;
+using Comfy.Domain.Identity;
+using Comfy.Tests.Mock;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+
+namespace Comfy.Tests.Users;
+
+public sealed class GetUserQueryHandlerTests
+{
+    private readonly IMapper _mapper;
+
+    public GetUserQueryHandlerTests()
+    {
+        _mapper = MapperMock.GetMapper();
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnUserDTO()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+        var store = new Mock<IUserStore<User>>();
+        store.Setup(x => x.FindByIdAsync(userId.ToString(), default)).ReturnsAsync(new User { Id = userId });
+        var userManagerMock = UserManagerMock.TestUserManager(store.Object);
+
+        var query = new GetUserQuery(userId);
+        var handler = new GetUserQueryHandler(_mapper, userManagerMock);
+
+        // Act
+        var result = await handler.Handle(query, default);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<UserDTO>();
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnNull()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+        var store = new Mock<IUserStore<User>>();
+        store.Setup(x => x.FindByIdAsync(userId.ToString(), default)).ReturnsAsync(new User { Id = userId });
+        var userManagerMock = UserManagerMock.TestUserManager(store.Object);
+
+        var query = new GetUserQuery(Guid.NewGuid());
+        var handler = new GetUserQueryHandler(_mapper, userManagerMock);
+
+        // Act
+        var result = await handler.Handle(query, default);
+
+        // Assert
+        result.Should().BeNull();
+    }
+}
