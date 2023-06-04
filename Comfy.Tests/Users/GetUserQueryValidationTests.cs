06ed9d6d2abf2f86c4a4a9f92d33f7363b27204d
tapeex <1lavabox1@gmail.com>
2023-06-04 20:34:28 +0300

    Добавил тесты на валидацию GetUserQuery

---
+++ Comfy.Tests/Users/GetUserQueryValidationTests.cs
@@ -0,0 +1,40 @@
+﻿using Comfy.Application.Handlers.Users;
+using Comfy.Application.Handlers.Users.Validators;
+using FluentAssertions;
+
+namespace Comfy.Tests.Users;
+
+public class GetUserQueryValidationTests
+{
+    [Fact]
+    public async Task Handle_Should_ReturnTrue()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+        var validator = new GetUserQueryValidator();
+
+        var query = new GetUserQuery(userId);
+
+        // Act
+        var validationResult = await validator.ValidateAsync(query);
+
+        // Assert
+        validationResult.IsValid.Should().Be(true);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnFalse()
+    {
+        // Arrange
+        var userId = Guid.Empty;
+        var validator = new GetUserQueryValidator();
+
+        var query = new GetUserQuery(userId);
+
+        // Act
+        var validationResult = await validator.ValidateAsync(query);
+
+        // Assert
+        validationResult.IsValid.Should().Be(false);
+    }
+}
