9ed4e064c84ed20f09d2ea5d9e5a3787b05a79d7
tapeex <1lavabox1@gmail.com>
2023-06-05 15:41:57 +0300

    Добавил LikeReviewsCommandHandlerTests

---
+++ Comfy.Tests/Reviews/LikeReviewsCommandHandlerTests.cs
@@ -0,0 +1,58 @@
+﻿using Comfy.Application.Common.Exceptions;
+using Comfy.Application.Handlers.Reviews.Reviews;
+using Comfy.Application.Interfaces;
+using Comfy.Domain.Models;
+using MockQueryable.Moq;
+using Moq;
+
+namespace Comfy.Tests.Reviews;
+
+public sealed class LikeReviewsCommandHandlerTests
+{
+    private readonly Mock<IApplicationDbContext> _contextMock;
+    public LikeReviewsCommandHandlerTests()
+    {
+        _contextMock = new Mock<IApplicationDbContext>();
+    }
+
+    [Fact]
+    public async Task Handle_Should_ThrowNotFoundException_WhenReviewIsNotFound()
+    {
+        // Arrange
+        var reviews = new List<Review>
+        {
+            new() { Id = 1 }
+        };
+        var reviewsMock = reviews.AsQueryable().BuildMockDbSet();
+        _contextMock.Setup(x => x.Reviews).Returns(reviewsMock.Object);
+
+        var command = new LikeReviewCommand(47, Guid.Empty);
+        var handler = new LikeReviewCommandHandler(_contextMock.Object);
+
+        // Act
+        // Assert
+        await Assert.ThrowsAsync<NotFoundException>(async () => await handler.Handle(command, default));
+    }
+
+    [Fact]
+    public async Task Handle_Should_IncreaseLikesNumber()
+    {
+        // Arrange
+        var likes = 5;
+        var reviews = new List<Review>
+        {
+            new() { Id = 47, Likes = likes }
+        };
+        var reviewsMock = reviews.AsQueryable().BuildMockDbSet();
+        _contextMock.Setup(x => x.Reviews).Returns(reviewsMock.Object);
+
+        var command = new LikeReviewCommand(47, Guid.Empty);
+        var handler = new LikeReviewCommandHandler(_contextMock.Object);
+
+        // Act
+        await handler.Handle(command, default);
+
+        // Assert
+        Assert.Equal(likes + 1, reviews.First().Likes);
+    }
+}
