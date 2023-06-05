10b1b7a19f9ea1cf7e7b6e8580d37e44187763f4
tapeex <1lavabox1@gmail.com>
2023-06-05 16:14:54 +0300

    Добавил: LikeReviewAnswerCommandHandlerTests DislikeReviewAnswerCommandHandlerTests

---
+++ Comfy.Tests/ReviewAnswers/DislikeReviewAnswerCommandHandlerTests.cs
@@ -0,0 +1,58 @@
+﻿using Comfy.Application.Common.Exceptions;
+using Comfy.Application.Handlers.Reviews.ReviewAnswers;
+using Comfy.Application.Interfaces;
+using Comfy.Domain.Models;
+using MockQueryable.Moq;
+using Moq;
+
+namespace Comfy.Tests.ReviewAnswers;
+
+public class DislikeReviewAnswerCommandHandlerTests
+{
+    private readonly Mock<IApplicationDbContext> _contextMock;
+    public DislikeReviewAnswerCommandHandlerTests()
+    {
+        _contextMock = new Mock<IApplicationDbContext>();
+    }
+
+    [Fact]
+    public async Task Handle_Should_ThrowNotFoundException_WhenReviewAnswerIsNotFound()
+    {
+        // Arrange
+        var reviewAnswers = new List<ReviewAnswer>
+        {
+            new() { Id = 1 }
+        };
+        var reviewAnswersMock = reviewAnswers.AsQueryable().BuildMockDbSet();
+        _contextMock.Setup(x => x.ReviewAnswers).Returns(reviewAnswersMock.Object);
+
+        var command = new DislikeReviewAnswerCommand(47, Guid.Empty);
+        var handler = new DislikeReviewAnswerCommandHandler(_contextMock.Object);
+
+        // Act
+        // Assert
+        await Assert.ThrowsAsync<NotFoundException>(async () => await handler.Handle(command, default));
+    }
+
+    [Fact]
+    public async Task Handle_Should_IncreaseDislikesNumber()
+    {
+        // Arrange
+        var dislikes = 5;
+        var reviewAnswers = new List<ReviewAnswer>
+        {
+            new() { Id = 47, Dislikes = dislikes }
+        };
+        var reviewAnswersMock = reviewAnswers.AsQueryable().BuildMockDbSet();
+        _contextMock.Setup(x => x.ReviewAnswers).Returns(reviewAnswersMock.Object);
+
+        var command = new DislikeReviewAnswerCommand(47, Guid.Empty);
+        var handler = new DislikeReviewAnswerCommandHandler(_contextMock.Object);
+
+        // Act
+        await handler.Handle(command, default);
+
+        // Assert
+        Assert.Equal(dislikes + 1, reviewAnswers.First().Dislikes);
+    }
+}
---
+++ Comfy.Tests/ReviewAnswers/LikeReviewAnswerCommandHandlerTests.cs
@@ -0,0 +1,58 @@
+﻿using Comfy.Application.Common.Exceptions;
+using Comfy.Application.Handlers.Reviews.ReviewAnswers;
+using Comfy.Application.Interfaces;
+using Comfy.Domain.Models;
+using MockQueryable.Moq;
+using Moq;
+
+namespace Comfy.Tests.ReviewAnswers;
+
+public sealed class LikeReviewAnswerCommandHandlerTests
+{
+    private readonly Mock<IApplicationDbContext> _contextMock;
+    public LikeReviewAnswerCommandHandlerTests()
+    {
+        _contextMock = new Mock<IApplicationDbContext>();
+    }
+
+    [Fact]
+    public async Task Handle_Should_ThrowNotFoundException_WhenReviewAnswerIsNotFound()
+    {
+        // Arrange
+        var reviewAnswers = new List<ReviewAnswer>
+        {
+            new() { Id = 1 }
+        };
+        var reviewAnswersMock = reviewAnswers.AsQueryable().BuildMockDbSet();
+        _contextMock.Setup(x => x.ReviewAnswers).Returns(reviewAnswersMock.Object);
+
+        var command = new LikeReviewAnswerCommand(47, Guid.Empty);
+        var handler = new LikeReviewAnswerCommandHandler(_contextMock.Object);
+
+        // Act
+        // Assert
+        await Assert.ThrowsAsync<NotFoundException>(async () => await handler.Handle(command, default));
+    }
+
+    [Fact]
+    public async Task Handle_Should_IncreaseLikesNumber()
+    {
+        // Arrange
+        var likes = 5;
+        var reviewAnswers = new List<ReviewAnswer>
+        {
+            new() { Id = 47, Likes = likes }
+        };
+        var reviewAnswersMock = reviewAnswers.AsQueryable().BuildMockDbSet();
+        _contextMock.Setup(x => x.ReviewAnswers).Returns(reviewAnswersMock.Object);
+
+        var command = new LikeReviewAnswerCommand(47, Guid.Empty);
+        var handler = new LikeReviewAnswerCommandHandler(_contextMock.Object);
+
+        // Act
+        await handler.Handle(command, default);
+
+        // Assert
+        Assert.Equal(likes + 1, reviewAnswers.First().Likes);
+    }
+}
