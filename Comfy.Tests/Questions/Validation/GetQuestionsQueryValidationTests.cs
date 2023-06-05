fb4b0364afff62ad3ec3a5f615ae1263c3cc2a43
tapeex <1lavabox1@gmail.com>
2023-06-05 17:12:23 +0300

    Добавил: LikeQuestionCommandValidationTests GetQuestionsQueryValidationTests DislikeQuestionCommandValidationTests

---
+++ Comfy.Tests/Questions/Validation/GetQuestionsQueryValidationTests.cs
@@ -0,0 +1,40 @@
+﻿using Comfy.Application.Handlers.Questions.Questions;
+using Comfy.Application.Handlers.Questions.Questions.Validators;
+using FluentAssertions;
+
+namespace Comfy.Tests.Questions.Validation;
+
+public class GetQuestionsQueryValidationTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2222)]
+    public async Task Handle_Should_ReturnTrue(int productId)
+    {
+        // Arrange
+        var validator = new GetQuestionsQueryValidator();
+        var query = new GetQuestionsQuery(productId, null, null);
+
+        // Act
+        var validationResult = await validator.ValidateAsync(query);
+
+        // Assert
+        validationResult.IsValid.Should().Be(true);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-2222)]
+    public async Task Handle_Should_ReturnFalse(int productId)
+    {
+        // Arrange
+        var validator = new GetQuestionsQueryValidator();
+        var command = new GetQuestionsQuery(productId, null, null);
+
+        // Act
+        var validationResult = await validator.ValidateAsync(command);
+
+        // Assert
+        validationResult.IsValid.Should().Be(false);
+    }
+}
---
+++ Comfy.Tests/Questions/Validation/LikeQuestionCommandValidationTests.cs
@@ -0,0 +1,40 @@
+﻿using Comfy.Application.Handlers.Questions.Questions;
+using Comfy.Application.Handlers.Questions.Questions.Validators;
+using FluentAssertions;
+
+namespace Comfy.Tests.Questions.Validation;
+
+public class LikeQuestionCommandValidationTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2222)]
+    public async Task Handle_Should_ReturnTrue(int userId)
+    {
+        // Arrange
+        var validator = new LikeQuestionCommandValidator();
+        var command = new LikeQuestionCommand(userId, Guid.Empty);
+
+        // Act
+        var validationResult = await validator.ValidateAsync(command);
+
+        // Assert
+        validationResult.IsValid.Should().Be(true);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-2222)]
+    public async Task Handle_Should_ReturnFalse(int userId)
+    {
+        // Arrange
+        var validator = new LikeQuestionCommandValidator();
+        var command = new LikeQuestionCommand(userId, Guid.Empty);
+
+        // Act
+        var validationResult = await validator.ValidateAsync(command);
+
+        // Assert
+        validationResult.IsValid.Should().Be(false);
+    }
+}
