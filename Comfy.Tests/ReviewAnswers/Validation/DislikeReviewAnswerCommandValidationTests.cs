e6041bb86828ce6b7bd325ff6a7dd4b60a0e5953
tapeex <1lavabox1@gmail.com>
2023-06-05 17:23:22 +0300

    Добавил: LikeReviewAnswerCommandValidationTests DislikeReviewAnswerCommandValidationTests

---
+++ Comfy.Tests/ReviewAnswers/Validation/DislikeReviewAnswerCommandValidationTests.cs
@@ -0,0 +1,40 @@
+﻿using Comfy.Application.Handlers.Reviews.ReviewAnswers;
+using Comfy.Application.Handlers.Reviews.ReviewAnswers.Validators;
+using FluentAssertions;
+
+namespace Comfy.Tests.ReviewAnswers.Validation;
+
+public class DislikeReviewAnswerCommandValidationTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2222)]
+    public async Task Handle_Should_ReturnTrue(int userId)
+    {
+        // Arrange
+        var validator = new DislikeReviewAnswerCommandValidator();
+        var command = new DislikeReviewAnswerCommand(userId, Guid.Empty);
+
+        // Act
+        var validationResult = await validator.ValidateAsync(command);
+
+        // Assert
+        validationResult.IsValid.Should().Be(true);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-2222)]
+    public async Task Handle_Should_ReturnFalse(int userId)
+    {
+        // Arrange
+        var validator = new DislikeReviewAnswerCommandValidator();
+        var command = new DislikeReviewAnswerCommand(userId, Guid.Empty);
+
+        // Act
+        var validationResult = await validator.ValidateAsync(command);
+
+        // Assert
+        validationResult.IsValid.Should().Be(false);
+    }
+}
