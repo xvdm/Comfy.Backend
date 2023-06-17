e36986882f36fb1092600642956641deeacbffd5
tapeex <1lavabox1@gmail.com>
2023-06-17 16:40:33 +0300

    Added Id and Code fields to the ShowcaseProductDto

--- Comfy.Application/Handlers/Products/DTO/ShowcaseProductDTO.cs
+++ Comfy.Application/Handlers/Products/DTO/ShowcaseProductDTO.cs
@@ -7,10 +7,12 @@ namespace Comfy.Application.Handlers.Products.DTO;
 
 public sealed record ShowcaseProductDTO : IMapWith<Product>
 {
+    public int Id { get; init; }
     public string Name { get; init; } = null!;
     public decimal Price { get; init; }
     public int DiscountAmount { get; init; }
     public int Amount { get; init; }
+    public int Code { get; init; }
     public double Rating { get; init; }
     public int ReviewsNumber { get; set; }
     public string Url { get; init; } = null!;
