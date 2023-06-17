ed76bf04fe5fcc41986817ef177f6024b7cbede4
tapeex <1lavabox1@gmail.com>
2023-06-17 21:59:52 +0300

    Added ConfirmEmailCommandValidator and SendConfirmationEmailCommandValidator

---
+++ Comfy.Application/Handlers/Email/Validators/ConfirmEmailCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Comfy.Application.Handlers.Email.Validators;
+
+public sealed class ConfirmEmailCommandValidator : AbstractValidator<ConfirmEmailCommand>
+{
+    public ConfirmEmailCommandValidator()
+    {
+        RuleFor(x => x.Email).EmailAddress();
+        RuleFor(x => x.ConfirmationCode).NotEmpty();
+    }
+}
---
+++ Comfy.Application/Handlers/Email/Validators/SendConfirmationEmailCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Comfy.Application.Handlers.Email.Validators;
+
+public sealed class SendConfirmationEmailCommandValidator :AbstractValidator<SendConfirmationEmailCommand>
+{
+    public SendConfirmationEmailCommandValidator()
+    {
+        RuleFor(x => x.Email).EmailAddress();
+        RuleFor(x => x.ConfirmationCode).NotEmpty();
+    }
+}
