345d9987c94b55030d660326daecf9f9707d4631
tapeex <1lavabox1@gmail.com>
2023-07-06 17:32:23 +0300

    Added GetUserQuestionsQueryValidator and GetUserReviewsQueryValidator

---
+++ Comfy.Application/Handlers/Questions/Questions/Validators/GetUserQuestionsQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Comfy.Application.Handlers.Questions.Questions.Validators;
+
+public sealed class GetUserQuestionsQueryValidator : AbstractValidator<GetUserQuestionsQuery>
+{
+    public GetUserQuestionsQueryValidator()
+    {
+        RuleFor(x => x.UserId).NotEqual(Guid.Empty);
+    }
+}
---
+++ Comfy.Application/Handlers/Reviews/Reviews/Validators/GetUserReviewsQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Comfy.Application.Handlers.Reviews.Reviews.Validators;
+
+public sealed class GetUserReviewsQueryValidator : AbstractValidator<GetUserReviewsQuery>
+{
+    public GetUserReviewsQueryValidator()
+    {
+        RuleFor(x => x.UserId).NotEqual(Guid.Empty);
+    }
+}
