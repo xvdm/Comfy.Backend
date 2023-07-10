c9ff4caa9e3fcde56f1a8bd9962f01bea9415920
tapeex <1lavabox1@gmail.com>
2023-07-10 16:59:09 +0300

    Added CreateOrderCommandValidator

---
+++ Comfy.Application/Handlers/Orders/Validators/CreateOrderCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Comfy.Application.Handlers.Orders.Validators;
+
+public sealed class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(x => x.Address).NotEmpty();
+        RuleFor(x => x.Email).EmailAddress();
+        RuleFor(x => x.City).NotEmpty();
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Patronymic).NotEmpty();
+        RuleFor(x => x.Surname).NotEmpty();
+        RuleFor(x => x.PhoneNumber).NotEmpty();
+        RuleFor(x => x.UserId).NotEqual(Guid.Empty);
+        RuleFor(x => x.ProductsIds).NotEmpty();
+        RuleForEach(x => x.ProductsIds).GreaterThan(0);
+    }
+}
